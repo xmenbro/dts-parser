b5ede2ec2df197493d952962e990a595768af12c
xmenbro <morozov.sereoja2020@gmail.com>
2026-07-15 18:53:11 +0300

    feat: add trim function

---
+++ parser.c
@@ -0,0 +1,24 @@
+#include "parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+
+// Remove unwanted spaces
+char* trim(char* str) {
+    // Skip space chars at the beginning of a string
+    while (isspace(str))
+        str++;
+    // Is the string empty now?
+    if (*str == 0)
+        return str;
+
+    // Skip space chars at the end of a string
+    char* end = str + strlen(str) - 1;
+    while (end > str && isspace(*end))
+        end--;
+
+    end[1] = '\0';
+    return str;
+}
---
+++ parser.h
@@ -0,0 +1,9 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "device_type.h"
+
+// Remove unwanted spaces
+char* trim(char* str);
+
+#endif
