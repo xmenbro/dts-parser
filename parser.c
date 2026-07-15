2a1334e7cb4bcc1c09be46d0ef00a1e62fed9f07
xmenbro <morozov.sereoja2020@gmail.com>
2026-07-15 19:09:38 +0300

    fix(parser.c): add conversion to unsigned char for correct function operation

--- parser.c
+++ parser.c
@@ -8,7 +8,7 @@
 // Remove unwanted spaces
 char* trim(char* str) {
     // Skip space chars at the beginning of a string
-    while (isspace(str))
+    while (isspace((unsigned char)*str))
         str++;
     // Is the string empty now?
     if (*str == 0)
@@ -16,7 +16,7 @@ char* trim(char* str) {
 
     // Skip space chars at the end of a string
     char* end = str + strlen(str) - 1;
-    while (end > str && isspace(*end))
+    while (end > str && isspace((unsigned char)*end))
         end--;
 
     end[1] = '\0';
